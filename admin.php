fdceaf9dd65d8d734b9af61bab6e20cb2e13a43d
Jordin Brown <jbrown@cognetif.com>
2019-05-10 16:24:22 -0400

    remove entry from perch sidebar menu

--- admin.php
+++ admin.php
@@ -3,7 +3,7 @@ $api            = new \PerchAPI(1.0, 'cognetif_apimail');
 
 if ($CurrentUser->logged_in()) {
 
-    $this->register_app('cognetif_apimail', 'ApiMail', 1, 'External Email API', '1.0', false);
+    $this->register_app('cognetif_apimail', 'ApiMail', 1, 'External Email API', '1.0', true);
     $this->require_version('cognetif_apimail', '3.0');
 
     require_once('util/autoloader.php');
